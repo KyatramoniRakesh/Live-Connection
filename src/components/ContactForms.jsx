39fa29a4784132a7c2c7c4e0cb7e293336a3a67f
KyatramoniRakesh <rakesh.rk1306@gmail.com>
2026-02-20 14:23:34 +0530

    final deployement

--- src/components/ContactForms.jsx
+++ src/components/ContactForms.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../CSS/ContactForms.css";
 import { FiChevronDown } from "react-icons/fi";
+// import HrResource from "./HrResource";
 import HrResource from "./HrResource";
 
 function ContactForms() {
